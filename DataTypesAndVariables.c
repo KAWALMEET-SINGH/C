356832aac0bf5ca709f99f0bd4a0b0e1e4d48a27
Kawalmeet Singh <kmssr782002@gmail.com>
2024-02-23 17:00:04 +0530

    var and  io operations

---
+++ DataTypesAndVariables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a = 2;
+    char b = 'b'; 
+    float c = 3.2;
+    double d = 0912424.41431;
+    
+    printf("%d\n", a); 
+    printf("%c\n", b); 
+    printf("%f\n", c); 
+    printf("%lf\n", d); 
+    
+    return 0; 
+}
